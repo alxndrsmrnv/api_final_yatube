444f707692983152528e5b88638b6ca04fc727c6
Alexander <ya.alexandersmirnov@yandex.ru>
2021-08-30 14:45:27 +0300

    1.0

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 
-from posts.models import Follow, Group, Post
+from posts.models import Group, Post
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
